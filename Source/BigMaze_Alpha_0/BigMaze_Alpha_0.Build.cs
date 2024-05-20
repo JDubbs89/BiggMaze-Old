0570e14eb8e2f069beb54fd4bc768a7430f60d89
JDubbs89 <152918687+JDubbs89@users.noreply.github.com>
2024-05-20 02:32:05 -0400

    connecting

--- Source/BigMaze_Alpha_0/BigMaze_Alpha_0.Build.cs
+++ Source/BigMaze_Alpha_0/BigMaze_Alpha_0.Build.cs
@@ -10,7 +10,11 @@ public class BigMaze_Alpha_0 : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] 
+		{ 
+			"SQLiteCore",
+			"SQLiteSupport"
+		});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
