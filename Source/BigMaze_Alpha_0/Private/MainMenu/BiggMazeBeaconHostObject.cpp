a360d8797f11f98d2b9e4a9e0bcda6e88ad9350a
JDubbs89 <152918687+JDubbs89@users.noreply.github.com>
2024-08-23 10:36:56 -0400

    g

--- Source/BigMaze_Alpha_0/Private/MainMenu/BiggMazeBeaconHostObject.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "BigMaze_Alpha_0/Public/MainMenu/BiggMazeBeaconHostObject.h"
-
-ABiggMazeBeaconHostObject::ABiggMazeBeaconHostObject()
-{
-
-}
--- Source/BigMaze_Alpha_0/Private/MainMenu/BiggMazeOnlineBeaconClient.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "BigMaze_Alpha_0/Public/MainMenu/BiggMazeOnlineBeaconClient.h"
-
-ABiggMazeOnlineBeaconClient::ABiggMazeOnlineBeaconClient()
-{
-
-}
